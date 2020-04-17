59620ac10ff103cf1a58097a5cc670dd2f4108bb
yaseengithub <shaik.yasinbasha@gmail.com>
2020-04-17 17:25:48 +0530

    testing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const auth = require('./platformData');
 const services = require('./platformApi');
 
+// git merge changes in asyncawite-test branch
+
 module.exports = {
     auth,
     ...services
