99646c42402e6579887e33f9ccdd2c4b3032c0a0
yaseengithub <shaik.yasinbasha@gmail.com>
2020-04-17 17:35:33 +0530

    fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const auth = require('./platformData');
 const services = require('./platformApi');
 
 // git merge changes in asyncawite-test branch
+// adding new changes
 
 module.exports = {
     auth,
